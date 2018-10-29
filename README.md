9b092315706ef50b2a193dada0f41d235d073018
wodkm <aa940209@163.com>
2018-10-30 01:44:37 +0800

    说明文档中增加Golang参考文献

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 golang Test for wodkm  
+Golang参考文献：https://www.golang123.com/book/9?chapterID=149
--- main/helloWorld.go
+++ main/helloWorld.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
+
+func cpuNo() int {
+	fmt.Println(runtime.NumCPU())
+	return runtime.NumCPU()
+}
 
 func main() {
 	fmt.Println("hello world")
+	cpuNo()
+	go cpuNo()
 }
